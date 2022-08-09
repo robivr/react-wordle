52f237474a4ddc38f99d1515a58bd41ddb3b34d3
robivr <vrbarobi@gmail.com>
2022-08-09 18:13:31 +0200

    fixed streak counting

--- src/components/ReactWordle.tsx
+++ src/components/ReactWordle.tsx
@@ -62,9 +62,10 @@ const ReactWordle = () => {
 
       if (won) {
         newState.won = newState.won + 1;
+        newState.last = 'won';
       }
 
-      if (won && state.last === 'won') {
+      if (won && newState.last === 'won') {
         newState.streak = newState.streak + 1;
         if (newState.streak > newState.maxStreak) {
           newState.maxStreak = newState.streak;
