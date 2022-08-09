b7ade73a932809fc4abf64c518be0fef2a891cc8
robivr <vrbarobi@gmail.com>
2022-08-09 18:09:54 +0200

    added desktop styling

--- src/components/Word/Letter.tsx
+++ src/components/Word/Letter.tsx
@@ -8,7 +8,7 @@ interface LetterProps {
 const Letter = (props: LetterProps) => {
   return (
     <div
-      className={`border flex items-center justify-center w-16 h-16 cursor-default bg-${props.color} text-white text-3xl`}
+      className={`border flex items-center justify-center w-16 h-16 cursor-default bg-${props.color} text-white text-3xl lg:w-20 lg:h-20 lg:text-4xl`}
     >
       {props.letter}
     </div>
