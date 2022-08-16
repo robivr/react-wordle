ccc226cc39f1b96c1b93eeccfebbf145c20911b4
robivr <vrbarobi@gmail.com>
2022-08-16 19:22:38 +0200

    updated chart

--- src/components/Chart/Bar.tsx
+++ src/components/Chart/Bar.tsx
@@ -3,19 +3,21 @@ import React from 'react';
 interface BarProps {
   label: string;
   barPercent: number;
+  barColor: string;
 }
 
 const Bar = (props: BarProps) => {
   const style = {
     '--bar-percent': `${props.barPercent}%`,
+    '--bar-color': props.barColor,
   } as React.CSSProperties;
 
   return (
-    <div className="bar flex">
-      1
+    <div className="bar flex items-center mb-1">
+      {props.label}
       <div
         style={style}
-        className="bg-black [width:var(--bar-percent)] h-8 min-h-full ml-4"
+        className="[background:var(--bar-color)] [width:var(--bar-percent)] h-8 min-h-full ml-4"
       ></div>
     </div>
   );
--- src/components/Chart/StatBarChart.tsx
+++ src/components/Chart/StatBarChart.tsx
@@ -6,12 +6,20 @@ interface BarChartProps {
 }
 
 const StatBarChart = (props: BarChartProps) => {
+  const total = props.stats.reduce((acc, val) => acc + val, 0);
+  const mostWins = props.stats.indexOf(Math.max(...props.stats));
+
   return (
     <div className="w-full">
       <h2 className="text-center mb-2">Stat graph</h2>
       <div className="graph mx-4">
-        {props.stats.map((bar, index) => (
-          <Bar label={index + 1 + ''} barPercent={50} />
+        {props.stats.map((barPercent, index) => (
+          <Bar
+            key={index}
+            label={index + 1 + ''}
+            barColor={index === mostWins ? '#6AAA64' : 'black'}
+            barPercent={(barPercent / total) * 100}
+          />
         ))}
       </div>
     </div>
